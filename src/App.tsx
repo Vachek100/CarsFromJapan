20abab87a94c895bd40b48de1c79162be228f983
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2023-11-17 21:52:46 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="grid h-screen place-items-center">
+        <h1>vite is awesome</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
