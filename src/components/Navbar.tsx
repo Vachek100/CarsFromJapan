5e098dcd86d5918067ac8e99585ab71352df44b7
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2023-11-18 15:11:39 +0100

    ProfileButton and added/changed styles to Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Container from "./ui/container";
 import CFJPNLogo from "@/assets/images/CFJPNLogo.png";
 import { HeartIcon, MoonIcon, SunIcon } from "@heroicons/react/20/solid";
 import { Button } from "./ui/button";
+import ProfileButton from "./ui/ProfileButton";
 
 const routes = [
   {
@@ -42,17 +43,7 @@ const Navbar = () => {
               </Button>
             ))}
           </div>
-          <div className="flex gap-5">
-            <Button
-              variant="default"
-              className="rounded-full bg-white px-5 py-3 font-bold text-[#020817] shadow-md hover:bg-pink-500 hover:text-white"
-            >
-              Log In
-            </Button>
-            <Button className="group relative rounded-full bg-white p-3 shadow-md hover:bg-pink-500">
-              <HeartIcon className="h-6 w-6 text-pink-500 group-hover:text-white" />
-              <div className="absolute bottom-0 right-0 flex h-[14px] w-[14px] translate-x-[25%] translate-y-[-175%] items-center justify-center rounded-full bg-[#fe0c10] font-bold shadow-sm"></div>
-            </Button>
+          <div className="flex items-center gap-5">
             <Button
               variant="ghost"
               className="rounded-full p-3 hover:bg-pink-500"
@@ -60,6 +51,7 @@ const Navbar = () => {
               <SunIcon className="h-6 w-6 text-white" />
               <MoonIcon className="hidden h-6 w-6 text-white" />
             </Button>
+            <ProfileButton />
           </div>
         </div>
       </Container>
---
+++ src/components/ui/ProfileButton.tsx
@@ -0,0 +1,44 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Zoropfp from "@/assets/images/zoropfp.jpg";
+
+const ProfileButton = () => {
+  const linkHoverStyles = "hover:text-pink-500 transition-colors";
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar className=" border-2 border-solid border-white transition-colors hover:border-pink-500">
+          <AvatarImage src={Zoropfp} />
+          <AvatarFallback>VK</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className={`${linkHoverStyles} cursor-pointer`}>
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuItem className={`${linkHoverStyles} cursor-pointer`}>
+          Billing
+        </DropdownMenuItem>
+        <DropdownMenuItem className={`${linkHoverStyles} cursor-pointer`}>
+          Favourites
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className={`${linkHoverStyles} cursor-pointer`}>
+          Log Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ProfileButton;
