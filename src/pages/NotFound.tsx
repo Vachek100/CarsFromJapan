e40e3662b63e97beee7fb59f904eb5119f9558b6
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2023-12-15 22:35:36 +0100

    adding stuf to DB

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import NotFoundPicture from "@/assets/images/NotFound.png";
+
 
 const NotFound = () => {
   const navigate = useNavigate();
   setTimeout(() => {
     navigate("/");
   }, 5000);
-  return <h1>404 NotFound</h1>;
+  return (
+    <div className="flex h-screen w-full items-center align-middle">
+      <img src={NotFoundPicture} alt="" />
+    </div>
+  );
 };
 
 export default NotFound;
