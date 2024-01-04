ac582c535552fdf933d228f22f0b6d4c365303a9
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-01-04 19:19:49 +0100

    added heart button

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Product } from "@/utilities/types";
 import formatCurrency from "@/utilities/formatCurrency";
+import { HeartIcon } from "@heroicons/react/20/solid";
 
 type ProductCard = {
   data: Product;
@@ -9,13 +10,16 @@ type ProductCard = {
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
   return (
     <div className="rounded-lg outline-0 ring-primary transition duration-300 hover:ring-2 focus:ring-2">
-      <Card className="group relative cursor-pointer rounded-lg border-2">
+      <Card className="relative cursor-pointer rounded-lg border-2">
         <CardContent className="pt-4">
           <div className="relative aspect-square rounded-lg bg-foreground/5 dark:bg-background">
+            <div className="group absolute z-10 translate-x-2 translate-y-2 rounded-full bg-white p-[.375rem]">
+              <HeartIcon className="h-7 w-7 text-gray-400 group-hover:text-pink-500" />
+            </div>
             <img
               src={data.imgUrl}
               alt=""
-              className="aspect-square rounded-lg object-cover transition-all duration-300 group-hover:scale-105"
+              className="aspect-square rounded-lg object-cover duration-300"
             />
           </div>
         </CardContent>
