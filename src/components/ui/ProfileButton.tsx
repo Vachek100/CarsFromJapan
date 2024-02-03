f18a9e8da48a27e8a298496bfebec72ff9b3ebda
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-02-03 23:16:18 +0100

    added no user pfp

--- src/components/ui/ProfileButton.tsx
+++ src/components/ui/ProfileButton.tsx
@@ -8,19 +8,29 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Zoropfp from "@/assets/images/zoropfp.jpg";
+import Questpfp from "@/assets/images/questpfp.jpg";
 import { Link } from "react-router-dom";
 import AuthModal from "../modals/AuthModal";
-
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
 
 const ProfileButton = () => {
+  const [user] = useAuthState(auth);
 
- 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar className=" border-2 border-solid border-white transition-colors hover:border-pink-500">
-          <AvatarImage src={Zoropfp} />
-          <AvatarFallback>VK</AvatarFallback>
+          {user ? (
+            <AvatarImage src={Zoropfp} />
+          ) : (
+            <AvatarImage src={Questpfp} />
+          )}
+          {user ? (
+            <AvatarFallback>VK</AvatarFallback>
+          ) : (
+            <AvatarFallback>QU</AvatarFallback>
+          )}
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
@@ -34,7 +44,7 @@ const ProfileButton = () => {
           <Link to="/favorites">Favorites</Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <p className="hover:bg-[#f1f5f9] px-2 py-1.5 text-sm cursor-pointer rounded-sm">
+        <p className="cursor-pointer rounded-sm px-2 py-1.5 text-sm hover:bg-[#f1f5f9]">
           <AuthModal />
         </p>
       </DropdownMenuContent>
