cf1b3bffb50c8f0d9550e8f771372e8e0b11cd4d
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-02-17 22:07:15 +0100

    deleted unnecessary things

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,20 +1,13 @@
 import Container from "@/components/ui/container";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/firebase";
 
 const Favorites = () => {
-  const [user] = useAuthState(auth);
   return (
     <Container>
       <div className="mb-4 mt-12 text-xl font-bold">Favorites</div>
-      {user ? (
-        <div>
-          <p>mazda rx-7</p>
-          <p>mazda rx-8</p>
-        </div>
-      ) : (
-        <p>You must login first to see your favorite cars</p>
-      )}
+      <div>
+        <p>mazda rx-7</p>
+        <p>mazda rx-8</p>
+      </div>
     </Container>
   );
 };
