69be4004c8ed7e61f8f6b8e5330993a1cfaeee57
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-02-23 23:25:04 +0100

    Added elements and styles to Favorites.tsx

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,13 +2,20 @@ import Container from "@/components/ui/container";
 
 const Favorites = () => {
   return (
-    <Container>
-      <div className="mb-4 mt-12 text-xl font-bold">Favorites</div>
-      <div>
-        <p>mazda rx-7</p>
-        <p>mazda rx-8</p>
+    <>
+      <div className="h-auto w-full bg-white py-7 shadow-md">
+        <Container>
+          <h1 className="text-[30px] font-bold">Favorites</h1>
+        </Container>
       </div>
-    </Container>
+      <Container>
+        <div className="mb-4 mt-12 text-xl font-bold">Favorites</div>
+        <div>
+          <p>mazda rx-7</p>
+          <p>mazda rx-8</p>
+        </div>
+      </Container>
+    </>
   );
 };
 
