be07bde74c5f9fdbca482a5e237ba820afca0f6f
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-02-23 22:18:50 +0100

    Added some elements with styles to Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,12 +2,19 @@ import Container from "@/components/ui/container";
 
 const Profile = () => {
   return (
-    <Container>
-      <h1 className="mt-12">Profile</h1>
-      <p>name</p>
-      <p>phone number</p>
-      <p>random thing</p>
-    </Container>
+    <>
+      <div className="h-auto w-full bg-white py-7 shadow-md">
+        <Container>
+          <h1 className="text-[30px] font-bold">Profile</h1>
+        </Container>
+      </div>
+      <Container>
+        <h1 className="mt-12">Profile</h1>
+        <p>name</p>
+        <p>phone number</p>
+        <p>random thing</p>
+      </Container>
+    </>
   );
 };
 
