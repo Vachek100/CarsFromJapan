e7ba70f239920e81e6823cbc41d9a0ecc3243190
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-02-26 13:32:35 +0100

    changed page Names

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,27 @@
+import Aside from "@/components/Aside";
+import ProfileSettingsHeader from "@/components/ProfileHeader";
+import Container from "@/components/ui/container";
+import { Outlet } from "react-router-dom";
+
+const Profile = () => {
+  return (
+    <>
+      <div className="h-auto w-full bg-white py-7 shadow-md">
+        <Container>
+          <h1 className="text-[30px] font-bold">Profile</h1>
+        </Container>
+      </div>
+      <Container>
+        <div className=" mt-10 rounded-md bg-white p-8 shadow-lg">
+          <ProfileSettingsHeader />
+          <div className="flex gap-2">
+            <Aside />
+            <Outlet />
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Profile;
