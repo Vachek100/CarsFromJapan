6bb2d2dc6a0bdf472955716227ac753b56da7b9d
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-02-26 13:29:30 +0100

    added aside into profile settings

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,13 +1,56 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Aside = () => {
+  const asideLinkStyle = "hover:underline underline-offset-4";
+
   return (
-    <div className="mt-8 flex flex-col gap-2">
-      <Link to="/profile">Profile</Link>
-      <Link to="/profile/account">Account</Link>
-      <Link to="/profile/appearance">Appearance</Link>
-      <Link to="/profile/notifications">Notifications</Link>
-      <Link to="/profile/display">Display</Link>
+    <div className="flex flex-col gap-2">
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "#6acef5" } : {};
+        }}
+        end
+        className={asideLinkStyle}
+        to="/profile"
+      >
+        Profile
+      </NavLink>
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "#6acef5" } : {};
+        }}
+        className={asideLinkStyle}
+        to="/profile/account"
+      >
+        Account
+      </NavLink>
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "#6acef5" } : {};
+        }}
+        className={asideLinkStyle}
+        to="/profile/appearance"
+      >
+        Appearance
+      </NavLink>
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "#6acef5" } : {};
+        }}
+        className={asideLinkStyle}
+        to="/profile/notifications"
+      >
+        Notifications
+      </NavLink>
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "#6acef5" } : {};
+        }}
+        className={asideLinkStyle}
+        to="/profile/display"
+      >
+        Display
+      </NavLink>
     </div>
   );
 };
