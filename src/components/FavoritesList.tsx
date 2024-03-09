15d3b121544b8ba978b6edeb26d9165a37f88b37
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-03-09 22:45:18 +0100

    renamed data for car

--- src/components/FavoritesList.tsx
+++ src/components/FavoritesList.tsx
@@ -9,7 +9,7 @@ const ProductList: React.FC<ProductListProps> = ({ items }) => {
   return (
     <div className="grid grid-cols-1 gap-4 small:grid-cols-2 medium:grid-cols-3 xl:grid-cols-4">
       {items.map((item) => (
-        <ProductCard key={item.id} data={item} />
+        <ProductCard key={item.id} car={item} />
       ))}
     </div>
   );
