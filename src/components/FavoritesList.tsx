f24468d68b77f35788ad41235f779cf98391410d
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-03-15 23:41:07 +0100

    added fetching

--- src/components/FavoritesList.tsx
+++ src/components/FavoritesList.tsx
@@ -1,18 +1,36 @@
-import ProductCard from "@/components/ui/ProductCard";
+import { useEffect, useState } from "react";
 import { DBCar } from "@/utilities/car";
+import FavoriteProductCard from "./ui/FavoriteProductCard";
+import { doc, getDoc } from "firebase/firestore";
+import { auth, firestore } from "@/firebase/firebase"; // import your firebase config
 
-type ProductListProps = {
-  items: DBCar[];
-};
+const FavoritesList: React.FC = () => {
+  const [likedCars, setLikedCars] = useState<DBCar[]>([]);
 
-const ProductList: React.FC<ProductListProps> = ({ items }) => {
+  useEffect(() => {
+    const fetchLikedCars = async () => {
+      const userId = auth.currentUser?.uid;
+      if (userId) {
+        const userRef = doc(firestore, "users", userId);
+        const userSnap = await getDoc(userRef);
+
+        if (userSnap.exists()) {
+          const userData = userSnap.data();
+          setLikedCars(userData?.likedCars);
+        }
+      }
+    };
+
+    fetchLikedCars();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 gap-4 small:grid-cols-2 medium:grid-cols-3 xl:grid-cols-4">
-      {items.map((item) => (
-        <ProductCard key={item.id} car={item} />
+      {likedCars.map((car) => (
+        <FavoriteProductCard key={car.id} car={car} />
       ))}
     </div>
   );
 };
 
-export default ProductList;
+export default FavoritesList;
