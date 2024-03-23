fd4678f0ced9166c8f1c305ad92a6f9795cda315
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-03-23 23:54:38 +0100

    now it shows current users pfp

--- src/components/ui/ProfileButton.tsx
+++ src/components/ui/ProfileButton.tsx
@@ -7,29 +7,47 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Zoropfp from "@/assets/images/zoropfp.jpg";
 import Questpfp from "@/assets/images/questpfp.jpg";
 import { NavLink } from "react-router-dom";
 import AuthModal from "../modals/AuthModal";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/firebase";
+import { auth, firestore } from "@/firebase/firebase";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
 
 const ProfileButton = () => {
   const [user] = useAuthState(auth);
+  const [profilePicture, setProfilePicture] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const user = auth.currentUser;
+      if (user) {
+        const userDoc = await getDoc(doc(firestore, "users", user.uid));
+        if (userDoc.exists()) {
+          setDisplayName(userDoc.data().displayName || "");
+          setProfilePicture(userDoc.data().profilePicture || "");
+        }
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar className=" border-2 border-solid border-white transition-colors hover:border-[#6acef5]">
           {user ? (
-            <AvatarImage alt="usersProfilePicture" src={Zoropfp} />
+            <AvatarImage alt="usersProfilePicture" src={profilePicture} />
           ) : (
             <AvatarImage alt="questsProfilePicture" src={Questpfp} />
           )}
           {user ? (
-            <AvatarFallback>VK</AvatarFallback>
+            <AvatarFallback className="text-xs">{displayName}</AvatarFallback>
           ) : (
-            <AvatarFallback>QU</AvatarFallback>
+            <AvatarFallback className="text-xs">Quest</AvatarFallback>
           )}
         </Avatar>
       </DropdownMenuTrigger>
